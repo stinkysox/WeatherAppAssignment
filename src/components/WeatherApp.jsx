900f5719ba3caac8a5f5fe29615e37f8733c27a0
Hevit <kopelahevit5@gmail.com>
2024-06-15 14:37:52 +0530

    updated

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -17,11 +17,38 @@ const WeatherApp = () => {
     location: "Canada",
   });
   const [isDarkMode, setIsDarkMode] = useState(false);
-
-  const [displayIcon, setDisplayIcon] = useState(snowImage);
+  const [weatherIcon, setWeatherIcon] = useState(snowImage);
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
+  };
+
+  const getWeatherIcon = (iconCode) => {
+    switch (iconCode) {
+      case "01d":
+      case "01n":
+        return clearImage;
+      case "02d":
+      case "02n":
+      case "03d":
+      case "03n":
+      case "04d":
+      case "04n":
+        return cloudImage;
+      case "09d":
+      case "09n":
+      case "10d":
+      case "10n":
+        return rainImage;
+      case "13d":
+      case "13n":
+        return snowImage;
+      case "50d":
+      case "50n":
+        return drizzleImage;
+      default:
+        return snowImage;
+    }
   };
 
   const handleGetWeatherDetails = async () => {
@@ -31,45 +58,37 @@ const WeatherApp = () => {
     try {
       const response = await fetch(api);
       const data = await response.json();
+      console.log(data);
 
+      const timestampUTC = new Date(data.dt * 1000);
+      console.log(timestampUTC);
+
+      const options = {
+        weekday: "short",
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        timeZoneName: "short",
+      };
+      const localTime = timestampUTC.toLocaleString("en-US", options);
+      console.log(localTime);
       setWeatherDetails({
         humidity: data.main.humidity + "%",
         wind: Math.floor(data.wind.speed) + " km/h",
         temperature: Math.floor(data.main.temp) + "°c",
         location: data.name,
+        dateTime: localTime,
       });
 
-      if (data.weather[0].icon === "01d" || data.weather[0].icon === "01n") {
-        setDisplayIcon(clearImage);
-      } else if (
-        data.weather[0].icon === "02d" ||
-        data.weather[0].icon === "02n" ||
-        data.weather[0].icon === "03d" ||
-        data.weather[0].icon === "03n" ||
-        data.weather[0].icon === "04d" ||
-        data.weather[0].icon === "04n" // Cloudy conditions
-      ) {
-        setDisplayIcon(cloudImage);
-      } else if (
-        data.weather[0].icon === "09d" ||
-        data.weather[0].icon === "09n" ||
-        data.weather[0].icon === "10d" || // Rainy conditions
-        data.weather[0].icon === "10n"
-      ) {
-        setDisplayIcon(rainImage);
-      } else if (
-        data.weather[0].icon === "13d" ||
-        data.weather[0].icon === "13n"
-      ) {
-        setDisplayIcon(snowImage); // snow conditions
-      } else if (
-        data.weather[0].icon === "50d" ||
-        data.weather[0].icon === "50n"
-      ) {
-        setDisplayIcon(drizzleImage); // Drizzle conditions
-      }
+      const iconCode = data.weather[0].icon;
+      const icon = getWeatherIcon(iconCode);
+      setWeatherIcon(icon);
     } catch (error) {
       console.log(error);
+      alert("Please enter a valid name");
     }
   };
 
@@ -78,11 +97,7 @@ const WeatherApp = () => {
   };
 
   return (
-
-    <h1>Weather App</h1>
-
     <div className={isDarkMode ? "dark-bg" : "weather-w-contianer"}>
-
       <div className="weather-app-bg">
         <div className="input-container">
           <input
@@ -98,7 +113,10 @@ const WeatherApp = () => {
         </div>
         <div>
           <div className="toggle-switch">
-            <button className="toggle-btn" onClick={toggleDarkMode}>
+            <button
+              className={isDarkMode ? "black-btn" : "toggle-btn"}
+              onClick={toggleDarkMode}
+            >
               {isDarkMode ? "Light" : "Dark"}
             </button>
           </div>
@@ -106,11 +124,22 @@ const WeatherApp = () => {
 
         <div className="temp-section">
           <div className="temp-image-container">
-            <img className="weather-image" src={displayIcon} alt="" />
+            <img className="weather-image" src={weatherIcon} alt="" />
           </div>
-          <div className="temp-text-container">
+          <div className={isDarkMode ? "dark-text" : "temp-text-container"}>
             <p>{weatherDetails.temperature}</p>
             <p>{weatherDetails.location}</p>
+            <p>{weatherDetails.dateTime}</p>
+          </div>
+          <div className="wind-humid-container">
+            <div className="humid-container">
+              <p> Humindity: {weatherDetails.humidity}</p>
+              <img className="small-icon" src={humidityIcon} alt="" />
+            </div>
+            <div className="wind-container">
+              <p>Wind: {weatherDetails.wind}</p>
+              <img className="small-icon" src={windImage} alt="" />
+            </div>
           </div>
         </div>
       </div>
